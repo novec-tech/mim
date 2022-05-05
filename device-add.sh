53d696f1c5148359a878ab72f2f8941ecf067355
novec.tech <90937877+novec-tech@users.noreply.github.com>
2022-05-05 03:02:57 +0200

    Update device-add.sh
    connected to airtable

--- device-add.sh
+++ device-add.sh
@@ -1,6 +1,6 @@
 # generete uid if not exist in /var/uid
 [ ! -f /var/mim.conf ] && openssl rand -hex 20 > /var/mim.conf
 
-curl -d @/var/mim.conf https://n8n.novec.tech/web\hook/fd8cede5-106c-49d1-92da-a34018ef1789
+curl -d @/var/mim.conf https://hooks.airtable.com/workflows/v1/genericWebhook/appJFrySXaUO3JXGF/wflcPpnxuEfmniBbm/wtrGFIjIyAn5BTsiZ
 
 
