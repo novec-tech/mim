58602573f122ac1d84999d6c8904c61bf9fb90cf
novec.tech <90937877+novec-tech@users.noreply.github.com>
2022-05-05 03:18:40 +0200

    Update device-add.sh

--- device-add.sh
+++ device-add.sh
@@ -1,7 +1,7 @@
 # generete uid if not exist in /var/uid
+# check for file /var/mim.conf and if not exist create new uid and send to airtable 
 [ ! -f /var/mim.conf ] && openssl rand -hex 20 > /var/mim.conf
-
-curl -d @/var/mim.conf https://hooks.airtable.com/workflows/v1/genericWebhook/appJFrySXaUO3JXGF/wflcPpnxuEfmniBbm/wtrGFIjIyAn5BTsiZ  -H "Accept: application/json"
+curl -d @/var/mim.conf https://hooks.airtable.com/workflows/v1/genericWebhook/appJFrySXaUO3JXGF/wflcPpnxuEfmniBbm/wtrGFIjIyAn5BTsiZ
 
 
 
